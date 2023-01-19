bd9ec71ef8e8acf6772c97a9cc4ab0a60e15ff47
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-01-19 15:54:47 +0200

    done pagination, need to do: axios+async/await, styles, simplelightbox lib

--- src/fetchImegesBixby.js
+++ src/fetchImegesBixby.js
@@ -1,14 +1,71 @@
-import Notiflix from 'notiflix';
-
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '32873967-2df471ca69613a4b365a872ae';
 const axios = require('axios').default;
 
-export default function getImeges(name) {
-    return fetch(`${BASE_URL}/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+// створюємо клас PixabayApiService з методом getImeges()
+export default class PixabayApiService {
+    constructor() {
+        this.inputValue = '';
+        this.page = 1;
+        this.perPage = 40;
+    }
+    getImeges() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const KEY = '32873967-2df471ca69613a4b365a872ae';
+    // console.log('До', this)
+    return fetch(`${BASE_URL}/?key=${KEY}&q=${this.inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.perPage}`)
         .then(resp => resp.json())
+        .then(data => {
+            // console.log(data);
+            // Збільшуємо значення this.page += 1
+                this.incrementPage()
+                //  console.log('Після', this)
+                // Повертаємо дата наперед в перший then
+                return data;
+                }
+        )
         .catch(err => console.error(err))   
+    }
+
+//     // Want to use async/await? Add the `async` keyword to your outer function/method.
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+    // Функція збільшення значення this.page
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1
+    }
+
+    get value() {
+        return this.inputValue
+    }
+
+    set value(newValue) {
+        this.inputValue = newValue
+    }
 }
+ 
+// 1. створюємо клас PixabayApiService з методом getImeges()
+// 2. імпортуємо в index.js
+// 3. потрібно, щоб з зовнішнього коду в цей клас вказувалась змінна this.inputValue,
+//     тому через гетер і сетер 
+//      get value() {
+//         return this.inputValue
+//     }
+
+//     set value(newValue) {
+//         this.inputValue = newValue (перезаписуємо значення в конструкторі)
+// }
+//     значення value приходить з зовн.коду через pixabayApiService.value
+    
+
 
 // export default function getImeges(name) {
 // axios.get(`${BASE_URL}/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
--- src/index.js
+++ src/index.js
@@ -1,41 +1,50 @@
 import './css/styles.css';
-import getImeges from './fetchImegesBixby'
+// імпортуємо клас PixabayApiService
+import PixabayApiService from './fetchImegesBixby'
 import Notiflix from 'notiflix';
 
-
 // const debounce = require('lodash.debounce');
-
 
 const refs = {
     formEl: document.querySelector('.search-form'),
     divEl: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('.load-more')
 }
+// створюємо об'єкт pixabayApiService на основі класу PixabayApiService
+const pixabayApiService = new PixabayApiService();
 
-refs.formEl.addEventListener('submit', onSearch);
+refs.formEl.addEventListener('submit', onSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore)
 
-function onSearch(evt) {
-    evt.preventDefault();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  refs.loadMoreBtn.classList.add('is-hidden')
+  if (evt.currentTarget.elements.searchQuery.value === '') {
+    Notiflix.Notify.failure('Sorry')
+    return;
+  }
   clearMarkup();
-  refs.loadMoreBtn.classList.remove("is-hidden")
-    const inputValue = evt.currentTarget.elements.searchQuery.value;
-    // console.dir(inputValue)
-
-  getImeges(inputValue)
-    .then(data => {
-      console.log(data);
-       if (data.hits.length === 0) {
-                Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-       } else {
-         createMarkup(data.hits)
-       }
-      
-     }
-      );
-  
-  
-    }
-
+  // refs.loadMoreBtn.classList.remove("is-hidden")
+  // через сетер класу PixabayApiService записуємо значення this.inputValue
+  pixabayApiService.value = evt.currentTarget.elements.searchQuery.value;
+  // Скидаємо методом resetPage() обєкта pixabayApiService this.page до 1
+  pixabayApiService.resetPage();
+// викликаємо метод getImeges об'єкта pixabayApiService і then-им проміс, який повертає цей метод
+  pixabayApiService.getImeges().then(data => {
+     if (data.hits.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+     } else {
+       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+      //  console.log(data);
+       createMarkup(data.hits);
+       refs.loadMoreBtn.classList.remove('is-hidden')
+       checkTotalHits(data);
+      }
+    
+  })
+ 
+}
 
 function createMarkup (dataArray) {
     const oneMarkup = obj => {
@@ -43,48 +52,62 @@ function createMarkup (dataArray) {
             <img src="${obj.webformatURL}" alt="${obj.tags}" loading="lazy" />
               <div class="info">
                 <p class="info-item">
-                 <b>${obj.likes}</b>
+                 <b>Likes: ${obj.likes}</b>
                 </p>
                 <p class="info-item">
-                  <b>${obj.views}</b>
+                  <b>Views: ${obj.views}</b>
                 </p>
                 <p class="info-item">
-                  <b>${obj.comments}</b>
+                  <b>Comments: ${obj.comments}</b>
                 </p>
                 <p class="info-item">
-                  <b>${obj.downloads}</b>
+                  <b>Downloads: ${obj.downloads}</b>
                 </p>
               </div>
           </div>`;
         }
 
     const markup = dataArray.map(oneMarkup).join('');
-    refs.divEl.insertAdjacentHTML('afterbegin', markup)
+    refs.divEl.insertAdjacentHTML('beforeend', markup)
+}
+
+function onLoadMore() {
+   pixabayApiService.getImeges().then(data => {
+     
+    //  console.log(data);
+
+
+    //  console.log(pixabayApiService.page);
+    //  console.log(pixabayApiService.perPage);
+    //  console.log(data.totalHits);
+     
+    //  console.log(mult)
+     createMarkup(data.hits);
+     checkTotalHits(data);
+
+    
+    
+  })
 }
 
 function clearMarkup() {
     refs.divEl.innerHTML = ''
 }
 
+function checkTotalHits(data) {
+  const mult = (pixabayApiService.page - 1) * pixabayApiService.perPage;
+  if (mult >= data.totalHits) {
+       refs.loadMoreBtn.classList.add('is-hidden');
+       Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")
+     }
+}
 
+// Плавне прокручування: ================================
+// const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
 
-
-
-
-/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div> */
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
